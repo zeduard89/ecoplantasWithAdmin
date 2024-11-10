3e59a621d28b7cd473fd92c11254e8b2aba1a641
Guillermo <zeduard89>
2024-11-10 16:52:08 -0300

    fix countMaceteros20 undfine

--- src/components/NavBar/ButtonCarrito.jsx
+++ src/components/NavBar/ButtonCarrito.jsx
@@ -8,8 +8,8 @@ const ButtonCarrito = ({onScroll }) => {
     const navigate = useNavigate();
 
     const { plantas = [], macetas = []  } = useSelector((state) => state.catalogo) || {};
-    const maceteros20 = useSelector((state)=> state.catalogo.countMaceteros20) ?? 0;
-    const maceteros30 = useSelector((state) => state.catalogo.countMaceteros30) ?? 0;
+    const maceteros20 = useSelector((state) => state.catalogo?.countMaceteros20 ?? 0);
+    const maceteros30 = useSelector((state) => state.catalogo?.countMaceteros30 ?? 0);
 
 
 
